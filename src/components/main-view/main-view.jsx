967d1ec3547d5c2564f4376e853d3e246418111f
eaadalen <eaadalen112@gmail.com>
2023-11-01 14:42:23 -0500

    3.4 revisions

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,12 +11,12 @@ export const MainView = () => {
     fetch("https://desolate-everglades-87695-c2e8310ae46d.herokuapp.com/movies")
       .then((response) => response.json())
       .then((data) => {
-        const moviesFromApi = data.docs.map((doc) => {
+        const moviesFromApi = data.map((doc) => {
           return {
-            id: doc.key,
-            title: doc.title,
-            image:`https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
-            author: doc.author_name?.[0]
+            id: doc._id,
+            title: doc.Title,
+            image: doc.ImagePath,
+            author: doc.Director.Name
           };
         });
         setMovies(moviesFromApi);
